bbc6427ecd8064d1af994ea71ed6919f8f7c0bae
Tomay0 <thomaspage33@gmail.com>
2021-06-29 23:40:20 +1200

    Routing error checks and added basic homepage

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,17 +1,36 @@
 require "messages"
 
 class StatsController < ApplicationController
-    def index
+    def get_server
         @id = params[:id]
-        @type = "None"
+        server = DiscordGuilds.where(guild_id: "eq.#{@id}").first
+        
+        if server.nil?
+            return false
+        end
+
+        @server_name = server.guild_name
+        return true
+    end
+    def index
+        if !get_server()
+            render "stats/404"
+            return
+        end
     end
     def user_messages
-        @id = params[:id]
+        if !get_server()
+            render "stats/404"
+            return
+        end
         returned_data = UserMessagesByGuild.where(guild_id: "eq.#{@id}")
         @data = Hash[returned_data.collect {|item| [item.user_id, {:name => item.user_name, :count => item.count}]}]
     end
     def channel_messages
-        @id = params[:id]
+        if !get_server()
+            render "stats/404"
+            return
+        end
         returned_data = MessagesByChannel.where(guild_id: "eq.#{@id}")
         @data = Hash[returned_data.collect {|item| [item.channel_id, {:name => item.channel_name, :count => item.count}]}]
     end
