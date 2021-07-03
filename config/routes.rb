feeacd72861045a5617ebc2d978096adbac243c7
Tomay0 <thomaspage33@gmail.com>
2021-07-03 17:17:43 +1200

    Added user disproportionate words

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/stats/:id/user_messages", to: "stats#user_messages", constraints: { id: /\d+/ }
   get "/stats/:id/channel_messages", to: "stats#channel_messages", constraints: { id: /\d+/ }
   get "/stats/:id/top_words", to: "stats#top_words", constraints: { id: /\d+/ }
+  get "/stats/:id/user_disproportionate_words", to: "stats#user_disproportionate_words", constraints: { id: /\d+/ }
   get "/stats/:id", to: redirect("/stats/%{id}/user_messages"), constraints: { id: /\d+/ }
   get "/stats/:id/*path", to: redirect("/stats/%{id}/user_messages"), constraints: { id: /\d+/ }
   get "*path", to: redirect("/")
